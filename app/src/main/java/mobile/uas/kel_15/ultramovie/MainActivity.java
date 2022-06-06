b6962e1358cff07f04f39c39b0e0dea90dc879c1
xeanth <56108002+xeanth8@users.noreply.github.com>
2022-06-06 13:31:32 +0700

    Overhaul navigation to bottom nav bar

--- app/src/main/java/mobile/uas/kel_15/ultramovie/MainActivity.java
+++ app/src/main/java/mobile/uas/kel_15/ultramovie/MainActivity.java
@@ -1,23 +1,21 @@
 package mobile.uas.kel_15.ultramovie;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    Toolbar toolbar;
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
+public class MainActivity extends AppCompatActivity {
+    //    Toolbar toolbar;
+//    DrawerLayout drawerLayout;
+//    NavigationView navigationView;
+    NavigationBarView navigationBarView;
 
     @SuppressLint("ResourceType")
     @Override
@@ -25,33 +23,68 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.main_toolbar);
-        setSupportActionBar(toolbar);
+        showFragment(new MovieFragment());
 
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigationView);
+        navigationBarView = findViewById(R.id.bottom_navigation);
+        navigationBarView.setOnItemSelectedListener(item -> {
+                    int id = item.getItemId();
 
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                toolbar,
-                R.string.openNavDrawer,
-                R.string.closeNavDrawer
+                    if (id == R.id.navigation_bottom_item_movies) {
+                        Toast.makeText(MainActivity.this, "Movies", Toast.LENGTH_SHORT).show();
+                        showFragment(new MovieFragment());
+                    } else if (id == R.id.navigation_bottom_item_genres) {
+                        Toast.makeText(MainActivity.this, "Genres", Toast.LENGTH_SHORT).show();
+                        showFragment(new GenreFragment());
+//                    Intent intent = new Intent(MainActivity.this, GenreActivity.class);
+//                    startActivity(intent);
+                    } else if (id == R.id.navigation_bottom_item_writers) {
+                        Toast.makeText(MainActivity.this, "Writers", Toast.LENGTH_SHORT).show();
+                        showFragment(new WriterFragment());
+                    } else if (id == R.id.navigation_bottom_item_more) {
+                        Toast.makeText(MainActivity.this, "More", Toast.LENGTH_SHORT).show();
+                        showFragment(new MoreFragment());
+                    }
+
+                    return true;
+                }
         );
 
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frame_main_content, fragment).commit();
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-        super.onPointerCaptureChanged(hasCapture);
     }
 }
+
+
+//        toolbar = findViewById(R.id.main_toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        drawerLayout = findViewById(R.id.drawer_layout);
+//        navigationView = findViewById(R.id.navigationView);
+//
+//        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+//                this,
+//                drawerLayout,
+//                toolbar,
+//                R.string.openNavDrawer,
+//                R.string.closeNavDrawer
+//        );
+//
+//        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+//        actionBarDrawerToggle.syncState();
+//        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+//    @Override
+//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//        return false;
+//    }
+//
+//    @Override
+//    public void onPointerCaptureChanged(boolean hasCapture) {
+//        super.onPointerCaptureChanged(hasCapture);
+//    }
