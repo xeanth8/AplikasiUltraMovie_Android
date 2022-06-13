0d39d3a63f37d0348abc3a1bef1c96226a6b859c
xeanth <56108002+xeanth8@users.noreply.github.com>
2022-06-14 04:49:36 +0700

    Dummy data cleaning up

--- app/src/main/java/mobile/uas/kel_15/ultramovie/dummy_to_be_removed/GenresDummy.java
+++
@@ -1,7 +0,0 @@
-package mobile.uas.kel_15.ultramovie.dummy_to_be_removed;
-
-public class GenresDummy {
-    public static String[] names = {
-            "Action", "Romance", "Drama"
-    };
-}
--- app/src/main/java/mobile/uas/kel_15/ultramovie/dummy_to_be_removed/WritersDummy.java
+++
@@ -1,7 +0,0 @@
-package mobile.uas.kel_15.ultramovie.dummy_to_be_removed;
-
-public class WritersDummy {
-    public static String[] names = {
-            "Paimon", "Aether", "Lumine"
-    };
-}
